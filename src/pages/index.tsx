41d8c34e5da3f3cd053471d3c2c97748dcc26265
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-01-27 14:21:48 -0300

    Componentes Layout e Titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={`
-      flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600
+      flex h-screen justify-center items-center bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
     `}>
-      <span className='text-4xl'>Texto</span>
+      <Layout titulo='Cadastro Simples'>
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
